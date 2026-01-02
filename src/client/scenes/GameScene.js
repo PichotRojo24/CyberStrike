20a3f5659a16da2d5c8ee367e0788d797b8b3817
JairoTorrijo <jairotorrijo@gmail.com>
2026-01-02 12:07:16 +0100

    Powerup

--- src/client/scenes/GameScene.js
+++ src/client/scenes/GameScene.js
@@ -25,6 +25,13 @@ preload() {
         this.isPaused = false;
         this.escWasDown = false;
         this.processor = new CommandProcessor();
+        this.powerUp = null;
+        this.powerUpTimer = 0;
+        this.playersPower = 
+        {
+        player1: false,
+         player2: false
+        };
     }
 
     create() {
@@ -68,12 +75,50 @@ preload() {
           this.physics.add.overlap(this.players.get('player1').sprite, this.outsideMap, this.scoreRightGoal, null, this);
         this.physics.add.overlap(this.players.get('player2').sprite, this.outsideMap, this.scoreLeftGoal, null, this);
 
+
+        this.time.addEvent({
+         delay: 5000,
+        loop: true,
+        callback: () => this.spawnPowerUp()
+        });
+
         this.escKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
 
 
     }
+    spawnPowerUp() {
+    // Si ya hay uno, no generes otro
+    if (this.powerUp) return;
+
+    const x = Phaser.Math.Between(100, 700);
+    const y = Phaser.Math.Between(200, 500);
+
+    this.powerUp = this.physics.add.sprite(x, y, 'PowerUp');
+    this.powerUp.setScale(0.5);
+    this.powerUp.body.allowGravity = false;
+
+    // Detectar recogida por cada jugador
+    this.players.forEach((paddle, id) => {
+        this.physics.add.overlap(
+            paddle.sprite,
+            this.powerUp,
+            () => this.collectPowerUp(id),
+            null,
+            this
+        );
+    });
+}
+collectPowerUp(playerId) {
+    console.log(playerId + " recogió el power up!");
+
+    this.playersPower[playerId] = true;
+
+    this.powerUp.destroy();
+    this.powerUp = null;
+}
 
       setUpPlayers() {
+        
         const leftPaddle = new Paddle(this, 'player1', 150, 300);
         const rightPaddle = new Paddle(this, 'player2', 600, 300);
 
@@ -84,6 +129,8 @@ preload() {
         const player2 = this.players.get('player2').sprite;
 
 
+        leftPaddle.id = 'player1';
+        rightPaddle.id = 'player2';
         this.physics.add.collider(player1, player2);
 
         const InputConfig = [
@@ -188,8 +235,18 @@ pushOpponent(pusher, target) {
         );
 
         if (distance < 100) {
-            const offsetX = Math.cos(angle) * 35;
-            const offsetY = Math.sin(angle) * 35;
+            let force = 35;
+
+        // Si el jugador tiene power-up, empuja más fuerte
+        if (this.playersPower[pusher.id]) 
+        {
+        force = 100; // fuerza aumentada
+         this.playersPower[pusher.id] = false; // se consume el power-up
+}
+
+const offsetX = Math.cos(angle) * force;
+const offsetY = Math.sin(angle) * force;
+
 
             target.sprite.x += offsetX;
             target.sprite.y += offsetY;
